b53830ca62834c5720958db9c436ec15866e07c4
Gabriel <gabriel.lamela.ch@gmail.com>
2024-04-23 12:57:45 +0200

    Crear los componentes de la app
    Crear carpetas diferentes para header, recipes y shoping-list
    Referenciar los commponentes en app.module.ts
    Renderizar los componentes en app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingListEditComponent } from './shoping-list/shoping-list-edit/shoping-list-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShopingListComponent,
+    ShopingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
