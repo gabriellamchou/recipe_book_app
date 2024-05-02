fb9036c72353659500c3910664fa68a9c834883b
Gabriel <gabriel.lamela.ch@gmail.com>
2024-05-02 12:41:38 +0200

    Sincronizar form en html con recipeForm en ts
    Declarar FormGroup (recipeForm) en ts
    Construir recipeForm con FormBuilder
    Poblar los campos en caso de que nos encontremos en editMode
    Ejecutar la construcción del recipeForm con nuestra suscripción a los parámetros de la ruta
    Sincronizar recipeForm con la etiqueta form en el html: [formGroup] = recipeForm
    Sincronizar cada campo del form con formControlName
    Implementar función onSubmit

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,8 +11,11 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id!: number;
   editMode: boolean = false;
+  recipeForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private fb: FormBuilder,
+              private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params
@@ -18,9 +23,31 @@ export class RecipeEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null;
-          console.log(this.editMode);
+          this.initForm();
         }
       );
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let recipeDescription = "";
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipeById(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.image;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = this.fb.group({
+      'name': recipeName,
+      'imagePath': recipeImagePath,
+      'description': recipeDescription
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
 }
