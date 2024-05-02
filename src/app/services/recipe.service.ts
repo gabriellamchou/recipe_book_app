30bc8611483d4516463afb85957f50707a800928
Gabriel <gabriel.lamela.ch@gmail.com>
2024-05-02 17:39:13 +0200

    Guardar nuevas recetas y ediciones

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
         new Recipe(
@@ -45,6 +47,16 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
 }
